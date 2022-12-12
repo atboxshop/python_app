282356bff58eced0a4ca16f5410e3b71c99159bb
Dark Alex <trananhtai@hotmail.com>
2022-12-12 12:45:04 +0000

    code changed

--- example4_2.py
+++ example4_2.py
@@ -1,6 +1,12 @@
-def square(number):
+"""def square(number):
     #Calcualate the square of number
     return number ** 2
 
 x = int(input("Enter any number: "))
-print("The square of this number is:", square(x))
+print("The square of this number is:", square(x))"""
+
+def square_root(number):
+    return number ** 0.5 #or number ** (1/2)
+
+x = square_root(int(input("Enter a number for calculating square root: ")))
+print("The square root of this number is:",x)
