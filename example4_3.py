6c2988adce1f813371128e5c17145de9349be76a
Dark Alex <trananhtai@hotmail.com>
2022-12-12 13:10:18 +0000

    Example4.3

---
+++ example4_3.py
@@ -0,0 +1,15 @@
+def maximum(value1, value2, value3):
+    #Return the maximum of tree values
+    max_value = value1
+    if value2 > max_value:
+        max_value = value2
+    if value3 > max_value:
+        max_value = value3
+    return max_value
+
+print("Enter three values for looking the biggest number in three entered values")
+x = int(input("Enter value 1:"))
+y = int(input("Enter value 2:"))
+z = int(input("Enter value 3:"))
+max = maximum(x,y,z)
+print("The biggest number is:",max)
