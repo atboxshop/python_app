11cb9cae33ab4d0273ae6d1970a03874dadcf27f
Dark Alex <trananhtai@hotmail.com>
2022-12-13 08:04:44 +0000

    message

--- fig04_02.py
+++ fig04_02.py
@@ -10,5 +10,38 @@ def roll_dice():
 
 def display_dice(dice):
     """Display one roll of the two dice"""
-    die1, die2 = dice
+    die1, die2 = dice # unpack the tuple into variables die1 and die2
     print(f'Player rolled {die1} + {die2} = {sum(dice)}')
+
+die_values = roll_dice() #first roll
+display_dice(die_values)
+
+#determine game staus and point, based on first roll
+sum_of_dice = sum(die_values)
+
+if sum_of_dice in (7,11): #win
+    game_status = "WON"
+elif sum_of_dice in (2, 3, 12): #lose
+    game_status = "LOST"
+else:
+    game_status = "CONTINUE"
+    my_point = sum_of_dice
+    print('Point is', my_point)
+
+#continue rolling until player wins or loses
+while game_status == "CONTINUE":
+    die_values = roll_dice()
+    display_dice(die_values)
+    sum_of_dice = sum(die_values)
+
+    if sum_of_dice == my_point:
+        game_status = "WON"
+    elif sum_of_dice == 7:
+        game_status = "LOST"
+
+#display "wins" or "lost" message
+if game_status == "WON":
+    print("Player wins")
+else:
+    print("Player loses")
+
