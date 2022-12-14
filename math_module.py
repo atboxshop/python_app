0aa4618d1213b6a56d62b624a28eaa11deac6516
Dark Alex <trananhtai@hotmail.com>
2022-12-14 14:24:32 +0000

    code

---
+++ math_module.py
@@ -0,0 +1,6 @@
+import math
+x = int(input("Enter a number for calculating square root: "))
+print(math.sqrt(x))
+
+y = int(input('Enter a number for calculating float absolute number: '))
+print(math.fabs(y))
